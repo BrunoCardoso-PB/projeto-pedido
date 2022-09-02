834c13f7aa0d5579ad43120ff3f2435f84ca7ec8
bruno <brunocardoso.ti@gmail.com>
2022-09-02 15:57:40 -0300

    aplicando modal nos itens corretos

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
-function iniciaModal(modalID){
-    const modal = document.getElementById(modalID)
+function inciaModal(e){
+    const modal = document.getElementById(e)
     modal.classList.add('mostrar')
+    modal.addEventListener('click', function(e){
+        if(e.target.id == 'modal-promocao'){
+            modal.classList.remove('mostrar')
+        }
+    })
 }
 
-const logo = document.querySelector('.logo')
-logo.addEventListener('click', function(){
-    iniciaModal('modal-promocao')
+
+const hamburguer1 = document.querySelector('#hamburguer1')
+hamburguer1.addEventListener('click', function(){
+    inciaModal('modal-promocao')
 })
 
+
+
+
+
+
+
+// function iniciaModal(e){
+//     const modal = document.getElementById(e)
+//     modal.classList.add('mostrar')
+//     modal.addEventListener('click', (e) => {
+//         if(e.target.id == 'modal-promocao'){
+//             modal.classList.remove('mostrar')
+//         }
+//     })
+// }
+
+// const logo = document.querySelector('.logo')
+// logo.addEventListener('click', function(){
+//     iniciaModal('modal-promocao')
+// })
