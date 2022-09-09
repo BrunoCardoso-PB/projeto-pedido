5cb535119adab15b82e0f436a151c0cace8a2dc1
Bruno Cardoso <brunocardoso.ti@gmail.com>
2022-09-08 21:37:39 -0300

    modal ajuste imagens e itens

--- script.js
+++ script.js
@@ -13,52 +13,93 @@ function iniciaModal(e) {
 const listaItens = document.querySelectorAll(".lista-itens");
 listaItens.forEach((item, index) => {
   item.addEventListener("click", function (e) {
-    if (e.currentTarget.id == "hamburguer1") {
-        alteraModal1()
+    if (e.currentTarget.id == "produto") {
+      alteraModal();
       iniciaModal("modal-promocao");
     }
-    if (e.currentTarget.id == "hamburguer2") {
-        alteraModal2()
-        iniciaModal("modal-promocao");
-        }    
-    })
-})
-
+    if (e.currentTarget.id == "produto2") {
+      alteraModal2();
+      iniciaModal("modal-promocao");
+    }
+    if (e.currentTarget.id == "produto3") {
+      alteraModal3();
+      iniciaModal("modal-promocao");
+    }
+  });
+});
 /*****************Alteração no modal para hamburguer 1*****************/
-
-function alteraModal1(){
-    const produto2 = document.querySelectorAll('#hamburguer1')
-    const arrayProduto2 = Array.from(produto2)
-    const objetoProduto = arrayProduto2.map((item)=>{
-        let h2 = item.querySelector('h2').innerText
-        let p = item.querySelector('p').innerText
-                
-    const lanches = document.querySelectorAll(".modal-container");      
-    const arrayLanches = Array.from(lanches);
-    const objetosLanches = arrayLanches.map((lanche) => {
-        let titulo = lanche.querySelector("h2")
-        titulo.innerText = h2;      
-        let descricao = lanche.querySelector('p')
-        descricao.innerText = p
-    }) 
-  })
+function alteraModal() {
+  const produto = document.querySelectorAll("#produto");
+  const arrayProduto = Array.from(produto).map((item) => {
+    let h2 = item.querySelector("h2").innerText;
+    let p = item.querySelector("p").innerText;
+    let precoAtual = item.querySelector("span.preco-atual").innerText;
+    let precoAntigo = item.querySelector("span.preco-antigo").innerText;
+    let imagem = item.querySelector('img').src
+    
+    const lanches = document.querySelectorAll(".modal-container");
+    const arrayLanches = Array.from(lanches).map((lanche) => {
+      let titulo = lanche.querySelector("h2");
+      titulo.innerText = h2;
+      let descricao = lanche.querySelector("p");
+      descricao.innerText = p;
+      let preco1 = lanche.querySelector("span.preco-atual");
+      preco1.innerText = precoAtual;
+      let preco2 = lanche.querySelector("span.preco-antigo");
+      preco2.innerText = precoAntigo;
+      let imagemAlterada = lanche.querySelector('img')
+      imagemAlterada.src = imagem
+    });
+  });
 }
-
 /*****************Alteração no modal para hamburguer 2*****************/
-function alteraModal2(){
-    const produto2 = document.querySelectorAll('#hamburguer2')
-    const arrayProduto2 = Array.from(produto2)
-    const objetoProduto = arrayProduto2.map((item)=>{
-        let h2 = item.querySelector('h2').innerText
-        let p  = item.querySelector('p').innerText 
-                
-    const lanches = document.querySelectorAll(".modal-container");      
-    const arrayLanches = Array.from(lanches);
-    const objetosLanches = arrayLanches.map((lanche) => {
-        let titulo = lanche.querySelector("h2")
-        titulo.innerText = h2;    
-        let descricao = lanche.querySelector('p')  
-        descricao.innerText = p;
-    }) 
-  })
+function alteraModal2() {
+  const produto = document.querySelectorAll("#produto2");
+  const arrayProduto = Array.from(produto).map((item) => {
+    let h2 = item.querySelector("h2").innerText;
+    let p = item.querySelector("p").innerText;
+    let precoAtual = item.querySelector("span.preco-atual").innerText;
+    let precoAntigo = item.querySelector("span.preco-antigo").innerText;
+    let imagem = item.querySelector('img').src
+
+    const lanches = document.querySelectorAll(".modal-container");
+    const arrayLanches = Array.from(lanches).map((lanche) => {
+      let titulo = lanche.querySelector("h2");
+      titulo.innerText = h2;
+      let descricao = lanche.querySelector("p");
+      descricao.innerText = p;
+      let preco1 = lanche.querySelector("span.preco-atual");
+      preco1.innerText = precoAtual;
+      let preco2 = lanche.querySelector("span.preco-antigo");
+      preco2.innerText = precoAntigo;
+      let imagemAlterada = lanche.querySelector('img')
+      imagemAlterada.src = imagem
+    });
+  });
+}
+alteraModal2();
+/***************Alteração no modal para hamburguer 3*******************/
+function alteraModal3() {
+  const produto = document.querySelectorAll("#produto3");
+  const arrayProduto = Array.from(produto).map((item) => {
+    let h2 = item.querySelector("h2").innerText;
+    let p = item.querySelector("p").innerText;
+    let precoAtual = item.querySelector("span.preco-atual").innerText;
+    let precoAntigo = item.querySelector("span.preco-antigo").innerText;
+    let imagem = item.querySelector('img').src
+
+    const lanches = document.querySelectorAll(".modal-container");
+    const arrayLanches = Array.from(lanches).map((lanche) => {
+      let titulo = lanche.querySelector("h2");
+      titulo.innerText = h2;
+      let descricao = lanche.querySelector("p");
+      descricao.innerText = p;
+      let preco1 = lanche.querySelector("span.preco-atual");
+      preco1.innerText = precoAtual;
+      let preco2 = lanche.querySelector("span.preco-antigo");
+      preco2.innerText = precoAntigo;
+      let imagemAlterada = lanche.querySelector('img')
+      imagemAlterada.src = imagem
+    });
+  });
 }
